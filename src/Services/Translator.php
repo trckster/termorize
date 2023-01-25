e43a73f470d8005b77ce30a08eff20c7278e554e
Daniil <daniil@vivaldi.net>
2023-01-26 01:42:58 +0800

    Fix codestyle

--- src/Services/Translator.php
+++ src/Services/Translator.php
@@ -16,38 +16,39 @@ class Translator
 
     public function translate(string $text): string
     {
-        if (Translation::query()->where('original_text', $text)->exists())
-           return Translation::query()->where('original_text', $text)->value('translation_text');
+        if (Translation::query()->where('original_text', $text)->exists()) {
+            return Translation::query()->where('original_text', $text)->value('translation_text');
+        }
 
-            $apiKey = env('YANDEX_TRANSLATOR_API_KEY');
+        $apiKey = env('YANDEX_TRANSLATOR_API_KEY');
 
-            $originTextLang = LanguageIdentifier::identify($text);
+        $originTextLang = LanguageIdentifier::identify($text);
 
-            if ($originTextLang === 'ru') {
-                $translationLang = 'en';
-            } else {
-                $translationLang = 'ru';
-            }
+        if ($originTextLang === 'ru') {
+            $translationLang = 'en';
+        } else {
+            $translationLang = 'ru';
+        }
 
-            $params = [
-                'key' => $apiKey,
-                'text' => $text,
-                'lang' => $translationLang,
-            ];
+        $params = [
+            'key' => $apiKey,
+            'text' => $text,
+            'lang' => $translationLang,
+        ];
 
-            $query = '?' . http_build_query($params);
+        $query = '?' . http_build_query($params);
 
-            $response = $this->httpClient->get("translate$query");
-            $responseContent = json_decode($response->getBody()->getContents(), true);
+        $response = $this->httpClient->get("translate$query");
+        $responseContent = json_decode($response->getBody()->getContents(), true);
 
-            $translationText = $responseContent['text'][0];
+        $translationText = $responseContent['text'][0];
 
-            Translation::query()->create([
-                'original_text' => $text,
-                'translation_text' => $translationText,
-                'original_lang' => $originTextLang,
-                'translation_lang' => $translationLang
-            ]);
+        Translation::query()->create([
+            'original_text' => $text,
+            'translation_text' => $translationText,
+            'original_lang' => $originTextLang,
+            'translation_lang' => $translationLang
+        ]);
 
         return $translationText;
     }
