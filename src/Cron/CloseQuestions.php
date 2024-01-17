f3918fb9fe06d0a723657403e1bc121e0b6229d0
Max XXX <maxonleb5@gmail.com>
2024-01-17 23:28:48 +0300

    Add template for pending task cron handler

---
+++ src/Cron/CloseQuestions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Termorize\Cron;
+
+use Termorize\Interfaces\CronCommand;
+
+class CloseQuestions implements CronCommand
+{
+    public function handle()
+    {
+        
+    }
+}
