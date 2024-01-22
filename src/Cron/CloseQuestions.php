ea1b406035a460684338f7a9214448f6f82203a4
Max XXX <maxonleb5@gmail.com>
2024-01-23 00:44:47 +0300

    Add SendQuestion class

--- src/Cron/CloseQuestions.php
+++ src/Cron/CloseQuestions.php
@@ -2,12 +2,24 @@
 
 namespace Termorize\Cron;
 
+use Carbon\Carbon;
 use Termorize\Interfaces\CronCommand;
+use Termorize\Models\PendingTask;
 
 class CloseQuestions implements CronCommand
 {
     public function handle()
     {
-        
+        $pendingTasks = PendingTask::query()
+            ->where(PendingTask::$scheduled_for, '=', Carbon::now())
+            ->get()
+            ->toArray();
+        /*
+        foreach($pendingTasks as $pendingTask)
+        {
+            $method = $pendingTask->method;
+
+            $
+        }*/
     }
 }
--- src/Tasks/SendQuestion.php
+++ src/Tasks/SendQuestion.php
@@ -2,6 +2,19 @@
 
 namespace Termorize\Tasks;
 
+use Longman\TelegramBot\Request;
+use Termorize\Models\PendingTask;
+use Termorize\Models\User;
+
 class SendQuestion
 {
+    public function handle(PendingTask $pendingTask)
+    {
+        $params = json_decode($pendingTask->parameters, true);
+        /*$user = User::query()->where('id', '=', $params[]);
+
+        Request::sendMessage([
+            'chat_id' =>
+        ])*/
+    }
 }
